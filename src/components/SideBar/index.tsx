33a54a24ab1c1814b610c36a5f6a3dff9cb694c0
Matthew Davies <114952251+vloermatt@users.noreply.github.com>
2022-10-31 23:47:47 +0200

    feat(opacity): add route & opacity initial page

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -51,6 +51,9 @@ const SideBar: React.FC = (props) => {
           <h1 className="sideLink" onClick={() => directTo('/color')}>
             Color
           </h1>
+          <h1 className="sideLink" onClick={() => directTo('/opacity')}>
+            Opacity
+          </h1>
         </div>
         <ContentWrapper>{children}</ContentWrapper>
       </div>
