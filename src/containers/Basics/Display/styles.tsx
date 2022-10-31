1753e9e35b6ca26ee540c579b29fb3d489a5f643
Matthew Davies <mdldavies@gmail.com>
2022-10-31 22:22:36 +0200

    feat(float): add interactive buttons

--- src/containers/Basics/Display/styles.tsx
+++ src/containers/Basics/Display/styles.tsx
@@ -1,44 +1,51 @@
 import styled from 'styled-components';
 
 export const BoxInline = styled.div`
-    display: inline;
-    padding: 30px;
-    margin: 10px;
-    border: 5px solid #403f4c;
-    background-color: #E1C9E4;
-    transition: 0.3s;
+  display: inline;
+  padding: 30px;
+  margin: 10px;
+  border: 5px solid #403f4c;
+  background-color: #e1c9e4;
+  transition: 0.3s;
 
-    &:hover {
-        margin: 30px;
-        padding: 50px;
-    }
+  &:hover {
+    margin: 30px;
+    padding: 50px;
+  }
 `;
 
 export const BoxInlineStill = styled.div`
-    display: inline;
-    padding: 30px;
-    margin: 10px;
-    border: 5px solid #403f4c;
-    background-color: #E1C9E4;
+  display: inline;
+  padding: 30px;
+  margin: 10px;
+  border: 5px solid #403f4c;
+  background-color: #e1c9e4;
 `;
 
 export const BoxInlineBlock = styled.div`
-    display: inline-block;
-    padding: 30px;
-    margin: 10px;
-    border: 5px solid #403f4c;
-    background-color: #E1C9E4;
-    transition: 0.3s;
+  display: inline-block;
+  padding: 30px;
+  margin: 10px;
+  border: 5px solid #403f4c;
+  background-color: #e1c9e4;
+  transition: 0.3s;
 
-    &:hover {
-        margin: 30px;
-        padding: 50px;
-    }
+  &:hover {
+    margin: 30px;
+    padding: 50px;
+  }
 `;
 
 export const BoxBlock = styled.div`
-    display: block;
-    border: 5px solid #403f4c;
-    background-color: #E1C9E4;
-    cursor: pointer;
+  display: block;
+  border: 5px solid #403f4c;
+  background-color: #e1c9e4;
+  cursor: pointer;
+`;
+
+export const BoxBox = styled.div`
+  padding: 30px;
+  margin: 10px;
+  border: 5px solid #403f4c;
+  background-color: #e1c9e4;
 `;
--- src/containers/Basics/Float/index.tsx
+++ src/containers/Basics/Float/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** Styling */
-import { Heading, TextCode, Text, Box } from '../../../themes/styledComponents';
+import { Heading, TextCode, Text, Box, Button, Row } from '../../../themes/styledComponents';
+import { FloatTypes } from '../../../utils/constants';
+import { BoxBox, BoxInline } from '../Display/styles';
 
 const Float: React.FC = () => {
+  // states
+  const [floatType, setFloatType] = useState<any>(FloatTypes.LEFT);
+  React.useEffect(() => {
+    console.log(floatType);
+  }, [floatType]);
   return (
     <React.Fragment>
       <Box
@@ -12,6 +19,24 @@ const Float: React.FC = () => {
         }}
       >
         <Heading>Float</Heading>
+        <Text style={{ marginTop: '50px' }}>
+          {`The `}
+          <TextCode>float</TextCode>
+          {` property specifies whether an element should float to the left, right, or not at all`}
+        </Text>
+        {Object.values(FloatTypes).map((type) => {
+          return (
+            <Button
+              key={type.valueOf()}
+              id={type.valueOf()}
+              style={{ marginTop: '20px', marginLeft: type !== FloatTypes.LEFT ? '20px' : 0 }}
+              onClick={() => setFloatType(type.valueOf())}
+            >
+              :{type.valueOf()}
+            </Button>
+          );
+        })}
+        <Box>{floatType && <BoxBox style={{ float: floatType }}>testing</BoxBox>}</Box>
       </Box>
     </React.Fragment>
   );
