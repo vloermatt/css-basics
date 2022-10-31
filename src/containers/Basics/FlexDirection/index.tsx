cadf3d8877fe0851d64bebb4fe114f52a2b90cec
George van Heerden <george.vanheerden@sovtech.com>
2022-10-31 23:59:09 +0200

    feat(flex-direction): updated flex driection contaier

--- src/containers/Basics/FlexDirection/index.tsx
+++ src/containers/Basics/FlexDirection/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Heading, PageWrapper } from '../../../themes/styledComponents';
+import React, { useState } from 'react';
+import { Button, Heading, PageWrapper } from '../../../themes/styledComponents';
 import { Box, Flex } from '@chakra-ui/react';
+
 const colors = ['blue', 'green', 'yellow', 'orange', 'purple'];
 const FlexDirection: React.FC = () => {
   const NewBox = (color: string) => <Box boxSize="200px" bg={color} />;
-
+  const [flexDirection, setFlexDirection] = useState<any>('row');
   return (
-    <PageWrapper>
+    <Box padding="20px">
       <Heading>:flex-direction</Heading>
-
-      <Flex gap="20px"></Flex>
-    </PageWrapper>
+      <Button onClick={() => setFlexDirection('row-reverse')}>Reverse Flex Direction</Button>
+      <Flex gap="20px" flexDirection={flexDirection}>
+        {colors.map((color) => NewBox(color))}
+      </Flex>
+    </Box>
   );
 };
 
